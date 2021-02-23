77bafc4a0c2b2de930bb0f44edb9383baa71b888
Joe Young <musicjoeyoung@gmail.com>
2021-02-22 23:29:26 -0500

    style/func: piano design; building Tone fn's

--- src/Piano.js
+++ src/Piano.js
@@ -1,9 +1,82 @@
 import React, { Component } from "react";
-
+import { playC4, playDb4, playD4 } from "./tone.fn.js";
 export class Piano extends Component {
   render() {
     return (
       <div className="main">
+        <div className="circle">D6</div>
+        <div className="circle">B4</div>
+        <div className="circle">B6</div>
+        <div className="circle">E1</div>
+        <div className="circle">F1</div>
+        <div className="circle">G1</div>
+        <div className="circle">A1</div>
+        <div className="circle">B1</div>
+        <div className="circle">C2</div>
+        <div className="circle">D2</div>
+        <div className="circle">F2</div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle">E2</div>
+        <div className="circle">G2</div>
+        <div className="circle">A2</div>
+        <div className="circle">B2</div>
+        <div className="circle">C3</div>
+        <div className="circle">E3</div>
+        <div className="circle">F3</div>
+        <div className="circle">G3</div>
+        <div className="circle">A3</div>
+        <div className="circle">B3</div>
+        <div className="circle">C4</div>
+        <div className="circle">D4</div>
+        <div className="circle">E4</div>
+        <div className="circle">F4</div>
+        <div className="circle">G4</div>
+        <div className="circle">A4</div>
+        <div className="circle">E8</div>
+        <div className="circle">C5</div>
+        <div className="circle">D5</div>
+        <div className="circle">F5</div>
+        <div className="circle">G5</div>
+        <div className="circle">A5</div>
+        <div className="circle">B5</div>
+        <div className="circle">C6</div>
+        <div className="circle">D6</div>
+        <div className="circle">E6</div>
+        <div className="circle">F6</div>
+        <div className="circle">G6</div>
+        <div className="circle">A6</div>
+        <div className="circle">C7</div>
+        <div className="circle">D7</div>
+        <div className="circle">E7</div>
+        <div className="circle">F7</div>
+        <div className="circle">G7</div>
+        <div className="circle">A7</div>
+        <div className="circle">B7</div>
+        <div className="circle">C8</div>
+        <div className="circle">D8</div>
+        <div className="circle">E8</div>
+        <div className="circle">F8</div>
+        <div className="circle">G8</div>
+        <div className="circle">A8</div>
+        <div className="circle">B8</div>
+        <div className="circle">F0</div>
+        <div className="circle">G0</div>
+        <div className="circle">A0</div>
+        <div className="circle">B0</div>
+        <div className="circle" onClick={playC4}>
+          C1
+        </div>
+        <div className="circle" onClick={playD4}>
+          D1
+        </div>
+        {/* black keys */}
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle" onClick={playDb4}>
+          xxxxDb4
+        </div>
         <div className="circle"></div>
         <div className="circle"></div>
         <div className="circle"></div>
@@ -43,30 +116,7 @@ export class Piano extends Component {
         <div className="circle"></div>
         <div className="circle"></div>
         <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="blackKey"></div>
+
         <div className="chromaticDivsMain">
           <div className="chromaticDivs"></div>
           <div className="chromaticDivs"></div>
---
+++ src/tone.fn.js
@@ -0,0 +1,417 @@
+import * as Tone from "tone";
+
+export function playF0() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("F0", "4n");
+}
+export function playGb0() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Gb0", "4n");
+}
+export function playG0() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("G0", "4n");
+}
+export function playAb0() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Ab0", "4n");
+}
+export function playA0() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("A0", "4n");
+}
+export function playBb0() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Bb0", "4n");
+}
+export function playB0() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("B0", "4n");
+}
+export function playC1() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("C1", "4n");
+}
+export function playDb1() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db1", "4n");
+}
+export function playD1() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("D1", "4n");
+}
+export function playEb1() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db1", "4n");
+}
+export function playE1() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("E1", "4n");
+}
+export function playF1() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("F1", "4n");
+}
+export function playGb1() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Gb1", "4n");
+}
+export function playG1() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("G1", "4n");
+}
+export function playAb1() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Ab1", "4n");
+}
+export function playA1() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("A1", "4n");
+}
+export function playBb1() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Bb1", "4n");
+}
+export function playB1() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("B1", "4n");
+}
+export function playC2() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("C2", "4n");
+}
+export function playDb2() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db2", "4n");
+}
+export function playD2() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("D2", "4n");
+}
+export function playEb2() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db2", "4n");
+}
+export function playE2() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("E2", "4n");
+}
+export function playF2() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("F2", "4n");
+}
+export function playGb2() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Gb2", "4n");
+}
+export function playG2() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("G2", "4n");
+}
+export function playAb2() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Ab2", "4n");
+}
+export function playA2() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("A2", "4n");
+}
+export function playBb2() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Bb2", "4n");
+}
+export function playB2() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("B2", "4n");
+}
+export function playC3() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("C3", "4n");
+}
+export function playDb3() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db3", "4n");
+}
+export function playD3() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("D3", "4n");
+}
+export function playEb3() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db3", "4n");
+}
+export function playE3() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("E3", "4n");
+}
+export function playF3() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("F3", "4n");
+}
+export function playGb3() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Gb3", "4n");
+}
+export function playG3() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("G3", "4n");
+}
+export function playAb3() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Ab3", "4n");
+}
+export function playA3() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("A3", "4n");
+}
+export function playBb3() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Bb3", "4n");
+}
+export function playB3() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("B3", "4n");
+}
+export function playC4() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("C4", "4n");
+}
+export function playDb4() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db4", "4n");
+}
+export function playD4() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("D4", "4n");
+}
+export function playEb4() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db4", "4n");
+}
+export function playE4() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("E4", "4n");
+}
+export function playF4() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("F4", "4n");
+}
+export function playGb4() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Gb4", "4n");
+}
+export function playG4() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("G4", "4n");
+}
+export function playAb4() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Ab4", "4n");
+}
+export function playA4() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("A4", "4n");
+}
+export function playBb4() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Bb4", "4n");
+}
+export function playB4() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("B4", "4n");
+}
+export function playC5() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("C5", "4n");
+}
+export function playDb5() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db5", "4n");
+}
+export function playD5() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("D5", "4n");
+}
+export function playEb5() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db5", "4n");
+}
+export function playE5() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("E5", "4n");
+}
+export function playF5() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("F5", "4n");
+}
+export function playGb5() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Gb5", "4n");
+}
+export function playG5() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("G5", "4n");
+}
+export function playAb5() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Ab5", "4n");
+}
+export function playA5() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("A5", "4n");
+}
+export function playBb5() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Bb5", "4n");
+}
+export function playB5() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("B5", "4n");
+}
+
+export function playC6() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("C6", "4n");
+}
+export function playDb6() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db6", "4n");
+}
+export function playD6() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("D6", "4n");
+}
+export function playEb6() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db6", "4n");
+}
+export function playE6() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("E6", "4n");
+}
+export function playF6() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("F6", "4n");
+}
+export function playGb6() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Gb6", "4n");
+}
+export function playG6() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("G6", "4n");
+}
+export function playAb6() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Ab6", "4n");
+}
+export function playA6() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("A6", "4n");
+}
+export function playBb6() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Bb6", "4n");
+}
+export function playB6() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("B6", "4n");
+}
+
+export function playC7() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("C7", "4n");
+}
+export function playDb7() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db7", "4n");
+}
+export function playD7() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("D7", "4n");
+}
+export function playEb7() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db7", "4n");
+}
+export function playE7() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("E7", "4n");
+}
+export function playF7() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("F7", "4n");
+}
+export function playGb7() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Gb7", "4n");
+}
+export function playG7() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("G7", "4n");
+}
+export function playAb7() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Ab7", "4n");
+}
+export function playA7() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("A7", "4n");
+}
+export function playBb7() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Bb7", "4n");
+}
+export function playB7() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("B7", "4n");
+}
+
+export function playC8() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("C8", "4n");
+}
+export function playDb8() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db8", "4n");
+}
+export function playD8() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("D8", "4n");
+}
+export function playEb8() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Db8", "4n");
+}
+export function playE8() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("E8", "4n");
+}
+export function playF8() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("F8", "4n");
+}
+export function playGb8() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Gb8", "4n");
+}
+export function playG8() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("G8", "4n");
+}
+export function playAb8() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Ab8", "4n");
+}
+export function playA8() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("A8", "4n");
+}
+export function playBb8() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("Bb8", "4n");
+}
+export function playB8() {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("B8", "4n");
+}
